69847ceafe60e07595918b53bda92979d8fb7b0a
unknown <grant.l.williams@outlook.com>
2024-06-03 17:42:48 -0400

    Made changes to main resume page, main resume css, and 404 page

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -18,7 +18,7 @@ export default function NotFoundPage() {
                 <h1>404</h1>
                 <h2>Page Not Found</h2>
                 <p>Looks like you may be lost Friend. Do you need help finding home?</p>
-                <Link to="/">Go Home</Link>
+                <p id="home" ><Link to="/">Go Home</Link></p>
             </section>
         </div>
     );
--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -14,66 +14,117 @@ export default function ResumePage() {
     }, []);
     return (
         <div id="full-stack-developer">
-            <section >
-                <h1>Full Stack Developer & Aerospace Engineering Student</h1>
-                <h2>Experience & Education</h2>
+            <section>       
+                <h1>Professional Experience</h1>
+                <h2>Freelance FullStack Developer</h2>
+                <h3>May 2024 - Present</h3>
                 <ul>
                     <li>
-                    <strong>Full Stack Developer/Aerospace Engineering</strong> - University of Central Florida/ Hillsborough Community College
-                    <span>August 2023 - Present</span>
-                    <p>Engaged in full-stack web development studies while advancing in Aerospace Engineering.</p>
+                        <strong>Code Development:</strong> Crafted software solutions tailored to specific client requirements.
                     </li>
-                </ul> 
-
-                <h2>Skills</h2>
+                    <li>
+                        <strong>Product Improvement:</strong> Provided strategic recommendations to enhance product functionality and align with client vision.
+                    </li>
+                    <li>
+                        <strong>System Optimization:</strong> Ensured systems operated at peak efficiency through performance tuning and optimization.
+                    </li>
+                    <li>
+                        <strong>Project Completion:</strong> Delivered high-quality solutions meeting client expectations and deadlines.
+                    </li>
+                </ul>
+                <br></br>
+                <h2>Sales Representative, Vivint</h2>
+                <h3>February 2021 - December 2023</h3>
                 <ul>
-                    <li>Cascading Style Sheets (CSS)</li>
-                    <li>JavaScript</li>
-                    <li>HTML5</li>
-                    <li>Web Development</li>
-                    <li>Node.js</li>
+                    <li>
+                        <strong>Marketing:</strong> Leveraged available resources to create effective self-advertisement campaigns.
+                    </li>
+                    <li>
+                        <strong>Client Management:</strong> Maintained a personalized client list, offering tailored solutions to meet individual needs.
+                    </li>
+                    <li>
+                        <strong>Merchandising:</strong> Demonstrated in-depth product knowledge to effectively sell and promote offerings.
+                    </li>
+                    <li>
+                        <strong>Communication:</strong> Understood and addressed customer needs, providing services that matched their requirements.
+                    </li>
+                    <li>
+                        <strong>Customer Service:</strong> Assisted customers in finding suitable products and services and maintaining customer satisfaction.
+                    </li>
+                </ul>
+                <br></br>
+                <h2>U.S. Army Sergeant (E-5)</h2>
+                <h3>September 2015 - February 2020</h3>
+                <ul>
+                    <li>
+                        <strong>Leadership:</strong> Led infantry teams of various sizes to successfully accomplish mission objectives.
+                    </li>
+                    <li>
+                        <strong>Training:</strong> Conducted comprehensive training programs to prepare team members for critical assignments.
+                    </li>
+                    <li>
+                        <strong>Planning:</strong> Coordinated manpower and equipment to efficiently complete objectives.
+                    </li>
+                    <li>
+                        <strong>Readiness:</strong> Maintained operational readiness of personnel and equipment for emergency response.
+                    </li>
+                    <li>
+                        <strong>Preparation:</strong> Utilized resources effectively to facilitate training, manage emergencies, and complete tasks promptly.
+                    </li>
+                    <li>
+                        <strong>Standardization:</strong> Developed standardized procedures for handling diverse situations, ensuring consistent responses across units.
+                    </li>
+                </ul>
+                <br></br>
+                <h1>Education</h1>
+                <h2>Full Stack Web Development Certificate</h2>
+                <h3>University of Central Florida</h3>
+                <p>Completed a comprehensive program covering front-end and back-end development, including JavaScript, HTML, CSS, Node.js, React.js, Express.js, MySQL, Handlebars.js, Bootstrap, MongoDB, Mongoose, and Sequelize.</p>
+                <ul>
+                    <strong>Technologies Learned:</strong>
                     <li>React.js</li>
+                    <li>Node.js</li>
                     <li>Express.js</li>
-                    <li>Mysql2</li>
+                    <li>MySQL2</li>
                     <li>Handlebars.js</li>
                     <li>Bootstrap</li>
                     <li>MongoDB</li>
                     <li>Mongoose</li>
                     <li>Sequelize</li>
-                    <li>Engineering Principles</li>
-
                 </ul>
-
-                <h2>Additional Experience</h2>
+                <br></br>
+                <h2>Associate of Science in Engineering</h2>
+                <h3>Hillsborough Community College</h3>
+                <p>Completed a two-year program in engineering, including courses in mathematics, physics, and engineering principles.</p>
+                <br></br>
+                <h2>Military Education</h2>
                 <ul>
-                    <li>
-                    <strong>Field Sales Representative</strong> - Vivint Smart Home 
-                    <span>February 2021 - December 2023</span>
-                    <p>Door-to-door sales for Vivint smart home security solutions.</p>
-                    </li>
+                    <li>Ranger School</li>
+                    <li>Ranger Assessment & Selection Program</li>
+                    <li>Infantry One Station Unit Training (OSUT)</li>
+                    <li>Basic Airborne course</li>
+                    <li>Basic Leadership Course</li>
                 </ul>
-
-                <h2>Military Service</h2>
+                <br></br>
+                <h1>Skills</h1>
                 <ul>
-                    <li>
-                        <strong>Gun Team Leader/Substitute Weapons Squad Leader</strong> - US Army
-                        <span>January 2019 - January 2020</span>
-                        <p>Performed leadership roles, including acting as Weapons Squad Leader when necessary.</p>
-                        <strong>Infantry Team Leader</strong> - US Army
-                        <span>January 2017 - January 2019</span>
-                        <p>Lead infantry personell through Air Field Seizures, Raids, Ambushes, Reconaissance missions.</p>
-                        <strong>Infantry Paratrooper</strong>
-                        <span>January 2015 - January 2017</span>
-                        <p>Performed infantry duties, including reconnaissance, security, and tactical movements.</p>
-                    </li>
+                    <li>Adaptable: Quick to adjust to new challenges and environments.</li>
+                    <li>Hard-Working: Committed to delivering high-quality results consistently.</li>
+                    <li>Resourceful: Skilled in finding innovative solutions to problems.</li>
+                    <li>Reflective: Regularly evaluates performance for continuous improvement.</li>
+                    <li>Flexible: Capable of handling multiple tasks and adapting to changing priorities.</li>
+                    <li>Disciplined: Maintains a strong sense of duty and responsibility.</li>
+                    <li>Reliable: Dependable and trustworthy in completing tasks.</li>
+                    <li>Resilient: Able to recover quickly from setbacks and challenges.</li>
+                    <li>Thorough: Ensures accuracy and completeness in all tasks.</li>
+                    <li>Persuasive: Effective in convincing others and driving results.</li>
+                    <li>Organized: Maintains a structured approach to managing tasks and projects.</li>
+                    <li>Accurate: Pays close attention to detail to ensure precision.</li>
+                    <li>Good Learner: Quickly grasps new concepts and skills.</li>
                 </ul>
-                <a href={backendURL2} target="_blank" rel="noopener noreferrer" className="download-resume">
-                    Download My Resume
-                </a>
-
+                <br></br>
+                
             </section>
-            
-
         </div>
     );
 }
